37b25ec39cee4da30f30ea3e45b12c2a6410e266
Provashish Roy <provashishroy95@gmail.com>
2022-05-12 16:52:16 +0600

    created some components and customze navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -11,8 +13,9 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
