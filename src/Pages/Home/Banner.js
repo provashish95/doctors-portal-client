45d0468113bea773aecbcde80ca4834adc713cb1
Provashish Roy <provashishroy95@gmail.com>
2022-05-12 17:39:50 +0600

    created cards by using daisy ui

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen">
+        <div class="hero min-h-screen px-12 bg-local background-image" >
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <h1 class="text-5xl font-bold">Your new smile starts here</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary ">Get Started</button>
                 </div>
             </div>
         </div>
