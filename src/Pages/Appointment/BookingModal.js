dc1c2de2b67285adad088dbb1a61ffee020257c7
Provashish Roy <provashishroy95@gmail.com>
2022-05-13 17:36:46 +0600

    load data on modal

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { format } from 'date-fns';
 
-const BookingModal = ({ treatment }) => {
-    const { name, slots } = treatment;
+const BookingModal = ({ treatment, date, setTreatment }) => {
+    const { _id, name, slots } = treatment;
+
+    const handleBooking = e => {
+        e.preventDefault();
+        const slot = e.target.slot.value;
+        console.log(slot, _id, name);
+        setTreatment(null);
+    }
     return (
         <div>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -9,10 +17,26 @@ const BookingModal = ({ treatment }) => {
                 <div className="modal-box">
                     <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-secondary">Booking for: {name}</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div className="modal-action">
-                        <label for="booking-modal" className="btn">Yay!</label>
-                    </div>
+
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 justify-items-center my-12'>
+                        <input type="text" disabled value={format(date, 'PP')} class="input w-full max-w-xs" />
+
+                        <select name="slot" class="select select-bordered w-full max-w-xs">
+                            {
+                                slots.map((slot, index) => <option
+                                    key={index}
+                                    value={slot}
+                                >{slot}</option>)
+                            }
+
+                        </select>
+
+                        <input type="text" name="name" placeholder="Your name" class="input w-full max-w-xs" />
+                        <input type="email" name="email" placeholder="Email" class="input w-full max-w-xs" />
+                        <input type="text" name="phone" placeholder="Phone" class="input w-full max-w-xs" />
+                        <input type="submit" value="Submit" class="btn btn-secondary w-full max-w-xs" />
+                    </form>
+
                 </div>
             </div>
         </div>
