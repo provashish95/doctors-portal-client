1c7192501ee70fa757277bbca18600ff77b1268e
Provashish Roy <provashishroy95@gmail.com>
2022-05-13 13:56:29 +0600

    load fake data on availableAppointments page

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,10 +1,22 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AvailableAppointments = ({ date }) => {
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => {
+                setServices(data)
+            })
+    }, [])
+
+
     return (
         <div>
-            Available Appointments on :  {format(date, 'PP')}
+            <h4 className='text-xl text-secondary text-center font-bold'> Available Appointments on :  {format(date, 'PP')}</h4>
+            <div></div>
         </div>
     );
 };
