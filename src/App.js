90683fdca280d81d2aa79916114365f87179a2b1
Provashish Roy <provashishroy95@gmail.com>
2022-05-13 13:36:36 +0600

    added appointment pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import NotFound from './Pages/NotFound/NotFound';
@@ -9,11 +10,12 @@ import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto px-12'>
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
