72c76cb99abf21351a062769d57cdea5260a935e
Provashish Roy <provashishroy95@gmail.com>
2022-05-17 00:22:28 +0600

    use react query

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -2,18 +2,27 @@ import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import BookingModal from './BookingModal';
 import Service from './Service';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 const AvailableAppointments = ({ date }) => {
-    const [services, setServices] = useState([]);
+    //const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
+    const formattedDate = format(date, 'PP');
+    const { isLoading, data: services } = useQuery(['available', formattedDate], () => fetch(`http://localhost:5000/available?date=${formattedDate}`)
+        .then(res => res.json()))
 
-    useEffect(() => {
-        fetch('http://localhost:5000/service')
-            .then(res => res.json())
-            .then(data => {
-                setServices(data)
-            })
-    }, [])
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+    /*  useEffect(() => {
+         fetch(`http://localhost:5000/available?date=${formattedDate}`)
+             .then(res => res.json())
+             .then(data => {
+                 setServices(data)
+             })
+     }, [formattedDate]) */
 
 
     return (
@@ -21,7 +30,7 @@ const AvailableAppointments = ({ date }) => {
             <h4 className='text-xl text-secondary text-center font-bold'> Available Appointments on :  {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-28'>
                 {
-                    services.map(service => <Service key={service._id} service={service} setTreatment={setTreatment}></Service>)
+                    services?.map(service => <Service key={service._id} service={service} setTreatment={setTreatment}></Service>)
                 }
             </div>
             {
