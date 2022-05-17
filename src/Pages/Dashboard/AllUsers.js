7c3f57a3f1b25a3a166c82fd5e2e56643f9e26ae
Provashish Roy <provashishroy95@gmail.com>
2022-05-18 02:39:41 +0600

    added user row component

--- src/Pages/Dashboard/AllUsers.js
+++ src/Pages/Dashboard/AllUsers.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import UserRow from './UserRow';
 
 const AllUsers = () => {
+    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/users').then(res => res.json()))
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h3>All Users</h3>
+            <h3>All Users : {users.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            users.map(user => <UserRow key={user._id} user={user}></UserRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
