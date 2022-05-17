a379573db78513f758f71dfda80298cd34c0da81
Provashish Roy <provashishroy95@gmail.com>
2022-05-17 11:53:26 +0600

    added myAppointments data on dashboard

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const MyAppointments = () => {
+    const [appointments, setAppointments] = useState();
+    const [user, loading] = useAuthState(auth);
+    let i = 1;
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/booking?patientEmail=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppointments(data))
+        }
+    }, [user]);
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
     return (
         <div>
-            <h4>My Appointments </h4>
+            <h4>My Appointments : {appointments?.length}</h4>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>SL</th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Treatment</th>
+                            <th>Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            appointments?.map(appointment => <tr key={appointment._id}>
+                                <th>{i++}</th>
+                                <td>{appointment.patientName}</td>
+                                <td>{appointment.date}</td>
+                                <td>{appointment.treatmentName}</td>
+                                <td>{appointment.slot}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
